0b76e3d03585851b178284db888d6ee8ecde9e55
Saswat Panda <45821961+spsaswat@users.noreply.github.com>
2026-03-23 11:02:57 +1100

    Add files via upload

---
+++ rospy_thread_fin_1.py
@@ -0,0 +1,246 @@
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import JointState
+from std_msgs.msg import UInt16
+
+from position_controller_ros.msg import GotoActionGoal
+from std_msgs.msg import Header
+from actionlib_msgs.msg import GoalID
+
+from datetime import datetime
+
+import time
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import threading
+import json
+import os
+
+# Initialize the global variable
+current_position = 0.0
+all_imgs = []
+
+# serial_number = 'f1230450' #L515
+# serial_number = '017322071325' #D435
+serial_number = '128422272123'  #D405
+
+save_fold_p = './data/test_plant_'
+
+now = datetime.now()
+dt_string = now.strftime("%Y%m%d%H%M%S")
+save_fold_p = save_fold_p + dt_string + '/'
+
+os.makedirs(save_fold_p, exist_ok=True) 
+
+def start_pipeline():
+    # Start streaming
+    pipeline.start(config)
+    # Get the depth sensor and set the visual preset
+
+
+def stop_pipeline():
+    # Stop streaming
+    pipeline.stop()
+    
+
+def save_intrinsics():
+    # Get the intrinsics
+    profile = pipeline.get_active_profile()
+    
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    depth_sensor.set_option(rs.option.visual_preset, 4)  # 4 High accuracy for D435 and D405 # 5 L515 short range 
+
+    # depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.color))  #change it back if it doesnot work
+    depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+    depth_intrinsics = depth_profile.get_intrinsics()
+
+    # Save the intrinsics in the required format
+    intrinsics_dict = {
+        "K": [
+            [depth_intrinsics.fx, 0, depth_intrinsics.ppx],
+            [0, depth_intrinsics.fy, depth_intrinsics.ppy],
+            [0, 0, 1]
+        ],
+        # Assuming that the distortion model is "Brown-Conrady",
+        # you can get the distortion parameters like this.
+        "dist": depth_intrinsics.coeffs,
+        "height": depth_intrinsics.height,
+        "width": depth_intrinsics.width
+    }
+
+    # Write the data to a .txt file
+    with open(save_fold_p+'kd_intrinsics.txt', 'w') as outfile:
+        json.dump(intrinsics_dict, outfile, indent=4)
+        
+        
+    #remove below code after intrinsics finalized
+    depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.color)) 
+    depth_intrinsics = depth_profile.get_intrinsics()
+
+    # Save the intrinsics in the required format
+    intrinsics_dict = {
+        "K": [
+            [depth_intrinsics.fx, 0, depth_intrinsics.ppx],
+            [0, depth_intrinsics.fy, depth_intrinsics.ppy],
+            [0, 0, 1]
+        ],
+        # Assuming that the distortion model is "Brown-Conrady",
+        # you can get the distortion parameters like this.
+        "dist": depth_intrinsics.coeffs,
+        "height": depth_intrinsics.height,
+        "width": depth_intrinsics.width
+    }
+
+    # Write the data to a .txt file
+    with open(save_fold_p+'kdc_intrinsics.txt', 'w') as outfile:
+        json.dump(intrinsics_dict, outfile, indent=4)
+
+
+def capture_images(pipeline, position):
+    # The pipeline is assumed to already be started
+    # time_0 = time.time()
+
+    # Capture one frame
+    frames = pipeline.wait_for_frames()
+
+    # time_1 = time.time()
+    # print((time_1 - time_0)*1000)
+
+
+    aligned_frames = align.process(frames)
+
+    # Get aligned depth and color frames
+    depth_frame = aligned_frames.get_depth_frame()
+    color_frame = aligned_frames.get_color_frame()
+
+    # time_2 = time.time()
+    # print((time_2 - time_0)*1000)
+
+    # Convert depth and color frames to numpy arrays
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    # time_3 = time.time()
+    # print((time_3 - time_0)*1000)
+
+    # Save images
+    position = int(position * 10**6)
+    
+#     Change accrding to camera requirements
+    all_imgs.append((color_image.copy(), save_fold_p+'rgb_'+str(position)+'.png'))
+    # cv2.imwrite(save_fold_p+'rgb_'+str(position)+'.png' , color_image)
+    cv2.imwrite(save_fold_p+'depth_'+str(position)+'.png', depth_image)
+    # all_imgs.append((depth_image.copy(), save_fold_p+'depth_'+str(position)+'.png'))
+
+    # time_4 = time.time()
+    # print((time_4 - time_0)*1000)
+
+def joint_states_callback(message):
+    global current_position
+    current_position = message.position[0]
+
+    # if current_position % 10 < 2:
+        # capture_images(current_position)
+
+# This function will publish a light command
+def switch_light(state):
+    light_command = UInt16()
+    light_command.data = state
+    light_publisher.publish(light_command)
+
+
+def start_moving_robot():
+
+    velocity = 0.038
+
+    cmd_vel_message = Twist()
+    cmd_vel_message.linear.x = velocity
+
+    cmd_vel_publisher.publish(cmd_vel_message)
+
+def stop_moving_robot():
+    cmd_vel_message = Twist()
+    cmd_vel_publisher.publish(cmd_vel_message)
+    
+def go_home():
+    # Build the message
+    msg = GotoActionGoal()
+    msg.header = Header()
+    msg.goal_id = GoalID()
+    msg.goal.position = 0.005
+    msg.goal.velocity = 0.2
+
+    # Make sure rospy is still running and then publish
+    # if not rospy.is_shutdown():
+    pub.publish(msg)
+    
+    
+# Configure the RealSense camera
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_device(serial_number)
+
+# config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30) #L515
+# config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30)  #L515
+# config.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30) #D435
+# config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)  #D435
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30) #D405
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)  #D405
+
+# Align depth frame to color frame
+align = rs.align(rs.stream.color)
+
+
+
+
+# Start pipeline and capture initial frames
+start_pipeline()
+
+# Save intrinsics
+save_intrinsics()
+
+for i in range(2):
+    frames = pipeline.wait_for_frames()
+    frames = pipeline.wait_for_frames()
+
+# Initialize your ROS node and other publishers/subscribers
+rospy.init_node('robot_controller')
+cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    
+light_publisher = rospy.Publisher('/write_pin', UInt16, queue_size = 10)
+
+joint_states_subscriber = rospy.Subscriber('/joint_states', JointState, joint_states_callback)
+
+# position driver publisher
+pub = rospy.Publisher('/go_to_position_server/goal', GotoActionGoal, queue_size=10)
+
+# if not rospy.is_shutdown() and current_position >= 0.006:
+#     go_home()
+
+
+try:
+    while not rospy.is_shutdown():
+        # if capture_images is running, skip this iteration
+        start_moving_robot()
+        print(current_position)
+        # give light command continously
+        #switch_light(1)
+        capture_images(pipeline, current_position)
+        # 1.65
+        if current_position != 0.0 and current_position >= 0.78:
+            stop_moving_robot()
+            # Turn off the light
+            #switch_light(0)
+            
+            print('All images captured, now saving')
+            # Save all remaining images using opencv
+            for img, name in all_imgs:
+                cv2.imwrite(name, img)
+            go_home()
+            break
+        capture_images(pipeline, current_position)
+
+except rospy.ROSInterruptException:
+    pass
